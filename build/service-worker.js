53e80423ffb9017ff49428f15ee92df89b5e7013
Warren Brown <warrengbrn@gmail.com>
2020-07-06 07:15:31 -0400

    fixed horizontal scrolling on mobile

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.067ee1a9fe43d986d266841cd19fbc5e.js"
+  "/Portfolio/precache-manifest.16acff34c4dddafe42af0cc6a5599e13.js"
 );
 
 self.addEventListener('message', (event) => {
